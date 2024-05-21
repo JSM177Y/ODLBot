6fece92786f3a6f403be32f8cf5992ea02bac965
JSM177Y <103542876+JSM177Y@users.noreply.github.com>
2024-05-21 16:49:49 -0400

    30 min

--- odl_youtube_.py
+++ odl_youtube_.py
@@ -41,7 +41,7 @@ async def on_ready():
     print(f'{bot.user.name} has connected to Discord!')
     check_new_video.start()  # Start the loop to check for new videos
 
-@tasks.loop(minutes=10)
+@tasks.loop(minutes = 30)
 async def check_new_video():
     try:
         print("Checking for new videos...")
